aeaad41e84aef9b7fdff84a730d9807cae07f577
TenuredCLOUD <85143513+TenuredCLOUD@users.noreply.github.com>
2025-01-16 11:30:02 -0700

    Compat ravage zombies rework
    - rename of  component zinfect to compat_rvgz for compat build structure
    
    - remove scheduled spawn referenced code, reworked into  waitAndExecute & cleaned main infection function
    
    - moved CBA_fnc_addClassEventHandler into post XEH for players
    
    - removed older ailments array structure (useless, now handles by newer inventory GUI)

---
+++ addons/compat_rvgz/script_component.hpp
@@ -0,0 +1,16 @@
+#define COMPONENT compat_rvgz
+#define COMPONENT_BEAUTIFIED Compat RVG Zombies
+#include "\z\misery\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_COMPAT_RVG_ZOMBIES
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_COMPAT_RVG_ZOMBIES
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_COMPAT_RVG_ZOMBIES
+#endif
+
+#include "\z\misery\addons\main\script_macros.hpp"
