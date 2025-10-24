6a6688245d9aeac9ac45158d27b1bf8d784fc0ff
TenuredCLOUD <85143513+TenuredCLOUD@users.noreply.github.com>
2025-10-24 11:42:47 -0700

    Sleep - Remove old bed models and improve conditions (#154)
    * Improve Sleep
    
    - removed old bed models in sleep component
    
    - added macro for bed objects (CUP included)
    
    - improved sleep condition, previous check worked but was sometimes a pain to get working (had to look at correct model positions)
    
    - newer sleep condition works with terrain models as well as placed sleeping objects, players can also now sleep inside some vehicles
    
    * Update addons/main/script_macros.hpp
    
    ---------
    
    Co-authored-by: Mike-MF <29175040+Mike-MF@users.noreply.github.com>

--- addons/sleep/script_component.hpp
+++ addons/sleep/script_component.hpp
@@ -2,8 +2,6 @@
 #define COMPONENT_BEAUTIFIED Sleep
 #include "\z\misery\addons\main\script_mod.hpp"
 
-#define MACRO_BED_MODELS "woodenbed_01_f.p3d", "bed_big_a.p3d", "bed_husbands.p3d", "vojenska_palanda.p3d", "postel_manz_kov.p3d", "sofa_01_f.p3d", "Land_ArmChair_01_F", "armchair.p3d", "armchair_big.p3d", "postel_panelak1.p3d", "postel_panelak2.p3d", "sleeping_bag_f.p3d", "sleeping_bag_blue_f.p3d", "sleeping_bag_brown_f.p3d", "ground_sheet_f.p3d", "ground_sheet_blue_f.p3d", "ground_sheet_khaki_f.p3d", "ground_sheet_opfor_f.p3d", "ground_sheet_yellow_f.p3d"
-
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 
