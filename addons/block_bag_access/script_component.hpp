33c8940e798faeb53fdbe642202649468bfdb4c4
TenuredCLOUD <85143513+TenuredCLOUD@users.noreply.github.com>
2025-11-16 17:26:14 -0700

    Add - Bag Access
    - added new component, bag_access
    
    - removes action to open bags from NPC's & players, prevents pick-pocketing and rating decrement on taking items from others inventories

---
+++ addons/block_bag_access/script_component.hpp
@@ -0,0 +1,16 @@
+#define COMPONENT block_bag_access
+#define COMPONENT_BEAUTIFIED Block Bag Access
+#include "\z\misery\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_BLOCK_BAG_ACCESS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_BLOCK_BAG_ACCESS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BLOCK_BAG_ACCESS
+#endif
+
+#include "\z\misery\addons\main\script_macros.hpp"
