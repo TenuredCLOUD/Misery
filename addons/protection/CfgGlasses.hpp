824e2687734d0c8b59f19bf4086356125ad9a81c
TenuredCLOUD <85143513+TenuredCLOUD@users.noreply.github.com>
2025-12-07 14:54:15 -0700

    Protection | Compat Ravage | Compat Remnant - Improve (#197)
    * Protection | Compat Ravage | Compat Remnant - Improve
    
    - tweaked protection values
    
    - added remnant survival suits
    
    - added many more classes to protection lists
    
    TODO:
    
    - add CSAT suits to protection uniforms, as well as more headgear for protective hearing
    
    * Add more protection gear
    
    - added many more classes to protection
    
    - fixed rebreather vests not showing up in arsenal with Mike's help (added all three variants)
    
    Co-Authored-By: Mike-MF <29175040+Mike-MF@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mike-MF <29175040+Mike-MF@users.noreply.github.com>

--- addons/protection/CfgGlasses.hpp
+++ addons/protection/CfgGlasses.hpp
@@ -5,8 +5,8 @@ class CfgGlasses {
         GVAR(gasmask) = 1;
         GVAR(scba) = 0;
         GVAR(skinProtection) = 0.25;
-        GVAR(respiratoryProtection) = 0.75;
-        GVAR(eyeProtection) = 0.25;
+        GVAR(respiratoryProtection) = 0.5;
+        GVAR(eyeProtection) = 1;
         GVAR(hearingProtection) = 0;
     };
 
@@ -15,9 +15,94 @@ class CfgGlasses {
         GVAR(scba) = 0;
         GVAR(skinProtection) = 0.25;
         GVAR(respiratoryProtection) = 0;
-        GVAR(eyeProtection) = 0.25;
+        GVAR(eyeProtection) = 1;
         GVAR(hearingProtection) = 0;
     };
+
+    // Respirators
+    class G_Respirator_blue_F: None {
+        GVAR(gasmask) = 0;
+        GVAR(scba) = 0;
+        GVAR(skinProtection) = 0;
+        GVAR(respiratoryProtection) = 0.25;
+        GVAR(eyeProtection) = 0;
+        GVAR(hearingProtection) = 0;
+    };
+
+    class G_Respirator_white_F: None {
+        GVAR(gasmask) = 0;
+        GVAR(scba) = 0;
+        GVAR(skinProtection) = 0;
+        GVAR(respiratoryProtection) = 0.25;
+        GVAR(eyeProtection) = 0;
+        GVAR(hearingProtection) = 0;
+    };
+
+    class G_Respirator_yellow_F: None {
+        GVAR(gasmask) = 0;
+        GVAR(scba) = 0;
+        GVAR(skinProtection) = 0;
+        GVAR(respiratoryProtection) = 0.25;
+        GVAR(eyeProtection) = 0;
+        GVAR(hearingProtection) = 0;
+    };
+
+    // Goggles
+    class G_Combat: None {
+        GVAR(gasmask) = 0;
+        GVAR(scba) = 0;
+        GVAR(skinProtection) = 0;
+        GVAR(respiratoryProtection) = 0;
+        GVAR(eyeProtection) = 0.75;
+        GVAR(hearingProtection) = 0;
+    };
+
+    class G_Combat_Goggles_tna_F: None {
+        GVAR(gasmask) = 0;
+        GVAR(scba) = 0;
+        GVAR(skinProtection) = 0;
+        GVAR(respiratoryProtection) = 0;
+        GVAR(eyeProtection) = 0.75;
+        GVAR(hearingProtection) = 0;
+    };
+
+    class G_EyeProtectors_F: None {
+        GVAR(gasmask) = 0;
+        GVAR(scba) = 0;
+        GVAR(skinProtection) = 0;
+        GVAR(respiratoryProtection) = 0;
+        GVAR(eyeProtection) = 0.75;
+        GVAR(hearingProtection) = 0;
+    };
+
+    class G_EyeProtectors_Earpeice_F: None {
+        GVAR(gasmask) = 0;
+        GVAR(scba) = 0;
+        GVAR(skinProtection) = 0;
+        GVAR(respiratoryProtection) = 0;
+        GVAR(eyeProtection) = 0.75;
+        GVAR(hearingProtection) = 0;
+    };
+
+    // Balaclavas
+    class G_Balaclava_TI_G_blk_F: None {
+        GVAR(gasmask) = 0;
+        GVAR(scba) = 0;
+        GVAR(skinProtection) = 0;
+        GVAR(respiratoryProtection) = 0;
+        GVAR(eyeProtection) = 0.75;
+        GVAR(hearingProtection) = 0;
+    };
+
+    class G_Balaclava_TI_G_tna_F: None {
+        GVAR(gasmask) = 0;
+        GVAR(scba) = 0;
+        GVAR(skinProtection) = 0;
+        GVAR(respiratoryProtection) = 0;
+        GVAR(eyeProtection) = 0.75;
+        GVAR(hearingProtection) = 0;
+    };
+
 
     // No Filter Variants
     class G_AirPurifyingRespirator_01_F;
@@ -26,7 +111,7 @@ class CfgGlasses {
         GVAR(scba) = 0;
         GVAR(skinProtection) = 0.25;
         GVAR(respiratoryProtection) = 0;
-        GVAR(eyeProtection) = 0.25;
+        GVAR(eyeProtection) = 1;
         GVAR(hearingProtection) = 0;
     };
 
@@ -36,7 +121,7 @@ class CfgGlasses {
         GVAR(scba) = 0;
         GVAR(skinProtection) = 0.25;
         GVAR(respiratoryProtection) = 0;
-        GVAR(eyeProtection) = 0.25;
+        GVAR(eyeProtection) = 1;
         GVAR(hearingProtection) = 0;
     };
 
@@ -46,7 +131,7 @@ class CfgGlasses {
         GVAR(scba) = 0;
         GVAR(skinProtection) = 0.25;
         GVAR(respiratoryProtection) = 0;
-        GVAR(eyeProtection) = 0.25;
+        GVAR(eyeProtection) = 1;
         GVAR(hearingProtection) = 0;
     };
 
@@ -56,7 +141,7 @@ class CfgGlasses {
         GVAR(scba) = 0;
         GVAR(skinProtection) = 0.25;
         GVAR(respiratoryProtection) = 0;
-        GVAR(eyeProtection) = 0.25;
+        GVAR(eyeProtection) = 1;
         GVAR(hearingProtection) = 0;
     };
 };
