88fb810669483a4f5463fff63aa3a16c7d05ac46
TenuredCLOUD <85143513+TenuredCLOUD@users.noreply.github.com>
2026-08-29 13:34:48 -0700

    Update config.cpp
    - no need for `ace_common` in config, that was during testing for `ace_common_fnc_registerItemReplacement` when exploring options for a single item swap logic

--- addons/compat_ims/config.cpp
+++ addons/compat_ims/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {QCLASSACE(common), QCLASS(common), "WBK_MeleeMechanics"};
+        requiredAddons[] = {QCLASS(common), "WBK_MeleeMechanics"};
         skipWhenMissingDependencies = 1;
         authors[] = {"TenuredCLOUD"};
         VERSION_CONFIG;
