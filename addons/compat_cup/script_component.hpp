9b5bfb3c85977cfb59f7a7015e51e640e67931a0
TenuredCLOUD <85143513+TenuredCLOUD@users.noreply.github.com>
2026-09-15 09:21:51 -0700

    Add - CUP compat (#345)
    - title, added CUP compat for building actions being used
    
    - removed `PumpHandle` user action from `Land_pumpa` class since it is automatically added to water pumps when using ACE buildings action setting

---
+++ addons/compat_cup/script_component.hpp
@@ -0,0 +1,16 @@
+#define COMPONENT compat_cup
+#define COMPONENT_BEAUTIFIED Compat CUP
+#include "\z\misery\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_COMPAT_CUP
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_COMPAT_CUP
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_COMPAT_CUP
+#endif
+
+#include "\z\misery\addons\main\script_macros.hpp"
